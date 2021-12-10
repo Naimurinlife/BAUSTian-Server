1987e8120cd13247806ac19831825cb7ae577786
Naimur Rahman <Naimurinlife@gmail.com>
2021-12-10 20:10:16 +0600

    Create Room Function Working

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
-
+from .forms import RoomForm
 # Create your views here.
 
 # rooms = [
@@ -22,3 +22,15 @@ def room(request, pk):
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
